4d3d4f6fe21b5916d8358c589e0fece53b34c3ee
John Patrick A. Delos Reyes <Kyonpon0503@gmail.com>
2024-10-11 14:27:01 +0800

    created foundation of nav-bar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,31 @@
+import { Container, Flex, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, cyan.400,blue.500)"}
+          bgClip={"text"}
+        >
+          <Link to={"/"}>CPE-4B</Link>
+        </Text>
+      </Flex>
+    </Container>
+  );
 };
 
 export default NavBar;
