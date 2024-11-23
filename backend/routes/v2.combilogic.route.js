11c7095677977d4d1a308a54d3c093a3a2105ab1
John Patrick A. Delos Reyes <Kyonpon0503@gmail.com>
2024-11-23 23:43:21 +0800

    added routes of first batch of API's

--- backend/routes/v2.combilogic.route.js
+++ backend/routes/v2.combilogic.route.js
@@ -1,8 +1,20 @@
 import express from "express";
-import { v2addCBcircuit } from "../controllers/v2.combilogic.controller.js";
+import {
+  v2addCBcircuit,
+  v2AddToContent,
+  v2DeleteCB,
+  v2DeleteContentById,
+  v2getAllCb,
+  v2UpdateCBById,
+} from "../controllers/v2.combilogic.controller.js";
 
 const router = express.Router();
 
 router.post("/createcb", v2addCBcircuit);
+router.get("/getall", v2getAllCb);
+router.put("/update/:id", v2UpdateCBById);
+router.delete("/delete/:id", v2DeleteCB);
+router.delete("/delete/:id/:contentId", v2DeleteContentById);
+router.put("/addcontent/:id", v2AddToContent);
 
 export { router as CBCircuitAPIv2 };
