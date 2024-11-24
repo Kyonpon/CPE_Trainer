df3f2df07a7c5a29b047799a1f4eee20dbf8bf65
John Patrick A. Delos Reyes <Kyonpon0503@gmail.com>
2024-11-24 13:41:31 +0800

    Removed old push database component for more clarity

--- frontend/src/pages/Createpage.jsx
+++ frontend/src/pages/Createpage.jsx
@@ -1,13 +1,15 @@
 import { Box } from "@chakra-ui/react";
 import PushDatabaseNewCircuit from "../components/PushDatabaseNewCircuit";
 import CreateHeading from "../components/CreateHeading";
+import PushDatabaseNewCircuit_try from "../components/Experimental/AllPushData";
 
 const Createpage = () => {
   return (
     <Box pt={10} display="flex">
       <Box display="flex" flexDirection="column" maxW={"container.sm"}>
         <CreateHeading></CreateHeading>
-        <PushDatabaseNewCircuit></PushDatabaseNewCircuit>
+
+        <PushDatabaseNewCircuit_try></PushDatabaseNewCircuit_try>
       </Box>
       <Box display="flex" flexDirection="column"></Box>
     </Box>
