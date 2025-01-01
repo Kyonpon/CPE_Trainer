b0f7ca3c98d750f76286fead2d06858d60a85bce
John Patrick A. Delos Reyes <Kyonpon0503@gmail.com>
2025-01-02 06:14:14 +0800

    Clean up createpage

--- frontend/src/pages/Createpage.jsx
+++ frontend/src/pages/Createpage.jsx
@@ -1,7 +1,5 @@
 import { Box } from "@chakra-ui/react";
-import PushDatabaseNewCircuit from "../components/PushDatabaseNewCircuit";
 import CreateHeading from "../components/CreateHeading";
-import PushDatabaseNewCircuit_try from "../components/Experimental/AllPushData";
 import CreateLib from "../components/Experimental/CreateLib";
 
 const Createpage = () => {
@@ -9,8 +7,6 @@ const Createpage = () => {
     <Box pt={10} display="flex">
       <Box display="flex" flexDirection="column" maxW={"container.sm"}>
         <CreateHeading></CreateHeading>
-
-        {/* <PushDatabaseNewCircuit_try></PushDatabaseNewCircuit_try> */}
         <CreateLib></CreateLib>
       </Box>
       <Box display="flex" flexDirection="column"></Box>
