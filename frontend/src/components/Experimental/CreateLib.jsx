76a056edbdb1b88e15ca038734c827a4732bf6a1
John Patrick A. Delos Reyes <Kyonpon0503@gmail.com>
2025-01-10 06:41:16 +0800

    MAJOR REVISION, made it dynamic to the three circuits

--- frontend/src/components/Experimental/CreateLib.jsx
+++ frontend/src/components/Experimental/CreateLib.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { useUlCircuits } from "../../hooks/zustandUlCircuit";
 import { useCBCircuits } from "../../hooks/zustandCBCircuit";
+import { useMicroCircuits } from "../../hooks/zustandMicroCircuit";
 import {
   Box,
   Button,
@@ -13,73 +14,40 @@ import {
 import AddText from "./AddText";
 
 function CreateLib() {
-  const location = useLocation(); //This is used to determined what Create is the page
+  const location = useLocation();
 
-  //#region States in Create
-  const [newUniversalLogicCircuit, setNewUniversalLogicCircuit] = useState({
-    universalCircuitName: "",
+  const circuitType = location.pathname.includes("createcb")
+    ? "combinational"
+    : location.pathname.includes("createmicro")
+    ? "microprocessor"
+    : "universal";
+
+  const [newCircuit, setNewCircuit] = useState({
+    circuitName: "",
     thumbnailUrl: "",
-    content: [
-      {
-        type: "Text",
-        text: "",
-      },
-    ],
+    content: [],
   });
 
-  const [newCombinationalLogicCircuit, setNewCombinationalLogicCircuit] =
-    useState({
-      //combinationalCircuitName: "",
-      circuitName: "",
-      thumbnailUrl: "",
-      content: [],
-    });
-
-  const [newMicroprocessorCircuit, setNewMicroprocessorCircuit] = useState({
-    microprocessorCircuitName: "",
-    thumbnailUrl: "",
-    content: [{ type: "Text", text: "" }],
-  });
-
-  //#endrgion
-
-  //#region Zustand calls
-  const { createUniversalLogicCircruit } = useUlCircuits();
-  const { createCombiLogicCircuit } = useCBCircuits();
-
-  //TO DO:
-  //const {createMicroCircuit} = useMicroCircuits();
+  const zustandFunctions = {
+    combinational: useCBCircuits().createCombiLogicCircuit,
+    universal: useUlCircuits().createUniversalLogicCircruit,
+    microprocessor: useMicroCircuits().createMicroCircuit,
+  };
 
   const handleAddNewCircuit = async () => {
-    const path = location.pathname;
-    if (path.includes("createcb")) {
-      const { success, message } = await createCombiLogicCircuit(
-        newCombinationalLogicCircuit
-      );
-      console.log("Success: ", success);
+    const createCircuit = zustandFunctions[circuitType];
+    if (createCircuit) {
+      const { success, message } = await createCircuit(newCircuit);
+      console.log("Success:", success);
       console.log("Message:", message);
-      console.log(newCombinationalLogicCircuit);
-    } else if (path.includes("createul")) {
-      const { success, message } = await createUniversalLogicCircruit(
-        newUniversalLogicCircuit
-      );
-      console.log("Success: ", success);
-      console.log("Message:", message);
-      console.log(newUniversalLogicCircuit);
-    } else if (path.includes("createmicro")) {
-      console.log("Create Micro");
+      console.log(newCircuit);
     }
   };
-  //#endregion
 
-  //#region CONTENT STUFF1
   const handleContentChange = (index, event, field = "text") => {
-    const updatedContent = [...newCombinationalLogicCircuit.content];
+    const updatedContent = [...newCircuit.content];
     updatedContent[index][field] = event.target.value;
-    setNewCombinationalLogicCircuit({
-      ...newCombinationalLogicCircuit,
-      content: updatedContent,
-    });
+    setNewCircuit((prev) => ({ ...prev, content: updatedContent }));
   };
 
   const handleAddContent = (type) => {
@@ -94,27 +62,21 @@ function CreateLib() {
       newContent.imageUrl = "";
       newContent.altText = "";
     }
-    setNewCombinationalLogicCircuit((prev) => ({
+    setNewCircuit((prev) => ({
       ...prev,
       content: [...prev.content, newContent],
     }));
   };
-  //#endregion
 
   const handleDeleteContent = (index) => {
-    const updatedContent = [...newCombinationalLogicCircuit.content];
-    updatedContent.splice(index, 1); // Remove content at the specified index
-    setNewCombinationalLogicCircuit({
-      ...newCombinationalLogicCircuit,
-      content: updatedContent,
-    });
+    const updatedContent = [...newCircuit.content];
+    updatedContent.splice(index, 1);
+    setNewCircuit((prev) => ({ ...prev, content: updatedContent }));
   };
 
   return (
     <Container maxW={"container.sm"}>
       <VStack spacing={10}>
-        {" "}
-        {/*This Might be redundant because it only contains one box*/}
         <Box
           w={"full"}
           bg={useColorModeValue("white", "gray.800")}
@@ -123,38 +85,34 @@ function CreateLib() {
           shadow={"md"}
         >
           <VStack spacing={4}>
-            {location.pathname.includes("createcb") && (
-              <>
-                <Input
-                  placeholder="Circuit Name"
-                  name="circuitName"
-                  value={newCombinationalLogicCircuit.circuitName}
-                  onChange={(event) =>
-                    setNewCombinationalLogicCircuit({
-                      ...newCombinationalLogicCircuit,
-                      circuitName: event.target.value,
-                    })
-                  }
-                />
-                <Input
-                  placeholder="Image URL"
-                  name="thumbnailUrl"
-                  value={newCombinationalLogicCircuit.thumbnailUrl}
-                  onChange={(event) =>
-                    setNewCombinationalLogicCircuit({
-                      ...newCombinationalLogicCircuit,
-                      thumbnailUrl: event.target.value,
-                    })
-                  }
-                />
-                <AddText
-                  content={newCombinationalLogicCircuit.content}
-                  onContentChange={handleContentChange}
-                  onAddContent={handleAddContent}
-                  onDeleteContent={handleDeleteContent}
-                />
-              </>
-            )}
+            <Input
+              placeholder="Circuit Name"
+              name="circuitName"
+              value={newCircuit.circuitName}
+              onChange={(event) =>
+                setNewCircuit({
+                  ...newCircuit,
+                  circuitName: event.target.value,
+                })
+              }
+            />
+            <Input
+              placeholder="Image URL"
+              name="thumbnailUrl"
+              value={newCircuit.thumbnailUrl}
+              onChange={(event) =>
+                setNewCircuit({
+                  ...newCircuit,
+                  thumbnailUrl: event.target.value,
+                })
+              }
+            />
+            <AddText
+              content={newCircuit.content}
+              onContentChange={handleContentChange}
+              onAddContent={handleAddContent}
+              onDeleteContent={handleDeleteContent}
+            />
             <Button colorScheme="blue" w="full" onClick={handleAddNewCircuit}>
               Create!
             </Button>
