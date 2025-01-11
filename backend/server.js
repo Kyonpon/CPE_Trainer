532115a2a64cc7fc2c8c70973abb12e8cd6a4570
John Patrick A. Delos Reyes <Kyonpon0503@gmail.com>
2025-01-11 10:54:16 +0800

    Updated to be uploaded to render

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import { CircuitAPI } from "./routes/circuits.route.js";
 import { UserAPI } from "./routes/user.route.js";
@@ -8,14 +9,13 @@ import { ULCircuitAPI } from "./routes/universallogic.route.js";
 import { CBCircuitAPIv2 } from "./routes/v2.combilogic.route.js";
 import { MicroCircuitAPI } from "./routes/micro.route.js";
 
-dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
 app.use(express.json());
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Server is ready!");
-});
 
 app.use("/api/circuits", CircuitAPI);
 app.use("/api/users", UserAPI);
@@ -23,7 +23,16 @@ app.use("/api/ulcircuits", ULCircuitAPI);
 app.use("/api/cbcircuits", CBCircuitAPIv2);
 app.use("/api/microcircuits", MicroCircuitAPI);
 
-app.listen(5000, () => {
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
   connectDB();
   console.log("Server started @ http://localhost:5000");
 });
