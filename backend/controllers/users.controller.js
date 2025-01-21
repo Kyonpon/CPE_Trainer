337b89b95d23aef498420ca229a7b738b774ac01
Kyonpon <kyonpon0503@gmail.com>
2025-01-21 23:01:59 +0800

    Added Admin Type in User and created the corresponding API

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -35,14 +35,33 @@ export const login = async (req, res) => {
   const isPassValid = await bcrypt.compare(password, user.password);
 
   if (!isPassValid) {
-    return res.json({ message: "Incorrect Username or Password!" });
+    return res.json({
+      success: false,
+      message: "Incorrect Username or Password!",
+    });
   }
 
   const token = jwt.sign({ id: user._id }, "secret");
   try {
-    res.status(201).json({ token, userID: user._id });
+    res.status(201).json({ success: true, token, userID: user._id });
   } catch (error) {
     console.log("Error!", error.message);
     res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const checkAdmin = async (req, res) => {
+  const { id } = req.params;
+
+  const user = await UserModel.findById(id);
+
+  if (!user) {
+    return res.status(404).json({ success: false, message: "User not found" });
+  }
+
+  try {
+    res.status(200).json({ success: true, userData: user });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "checkAdmin error" });
   }
 };
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,12 +2,18 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import UserModel from "../models/user.model.js";
-import { register, login } from "../controllers/users.controller.js";
+import {
+  register,
+  login,
+  checkAdmin,
+} from "../controllers/users.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 
 router.post("/login", login);
+
+router.get("/checkadmin/:id", checkAdmin);
 
 export { router as UserAPI };
