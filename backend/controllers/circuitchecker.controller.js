b1997747a85acb89861790010dec5ff5852bdc9a
MIS Patrick <jpdrprofessional@gmail.com>
2025-04-02 14:50:37 +0800

    created format for the result data from MCU

--- backend/controllers/circuitchecker.controller.js
+++ backend/controllers/circuitchecker.controller.js
@@ -1,5 +1,26 @@
 let receivedTruthTable = {};
 
+//Sample return data from esp 32
+// let resultTruthTable = {
+//   modulename1: {
+//     isPassed: [1,1,1,1],
+//     outputsActual:{
+//       fn1: [1,0,1,0],
+//       fn2: [1,1,1,1,1],
+//     }
+//   },
+//   modulename2: {
+//     isPassed: [0,0,0,0],
+//     outputsActual:{
+//       fn1: [1,0,1,0],
+//       fn2: [1,1,1,1],
+//     }
+//   }
+// };
+
+let resultTruthTable ={};
+
+// react app > Server
 export const getModuleCheckTable = (req, res) => {
   const { moduleName, inputs, outputs } = req.body;
 
@@ -9,6 +30,7 @@ export const getModuleCheckTable = (req, res) => {
   console.log("receivedTruthTable: ", receivedTruthTable);
 };
 
+// Server > ESP32
 export const testSend = (req, res) => {
   const { modulename } = req.params;
 
@@ -22,3 +44,9 @@ export const testSend = (req, res) => {
   console.log(processedTruthTable);
   res.json(processedTruthTable);
 };
+
+// ESP32 > Server
+export const getTestResults = (req, res) => {
+  const {moduleName, isPassed, outputsActual} = req.body;
+  resultTruthTable[moduleName] = { isPassed, outputsActual };
+}
